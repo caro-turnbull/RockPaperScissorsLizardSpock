70736aec8efd80ef80c102f3023f0cd9713ab9cb
caro-turnbull <caro.turnbull@gmail.com>
2023-11-08 12:51:49 -0500

    settingup components

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const options3 = [
+  {
+    number: 0,
+    name: 'Rock',
+    img: 'public/hands/fist.svg'
+  },
+  {
+    number: 1,
+    name: 'Paper',
+    img: 'public/hands/hand.svg'
+  },
+  {
+    number: 2,
+    name: 'Scissors',
+    img: 'public/hands/scissors.svg'
+  }
+];
+
+export const options5 = [
+  ...options3,
+  {
+    number: 3,
+    name: 'Lizard',
+    img: 'public/hands/lizard.svg'
+  },
+  {
+    number: 4,
+    name: 'Spock',
+    img: 'public/hands/vulcan.svg'
+  }
+];
+
+// export default { options3, options5 };
